66e0ac5fe17bfbcaef49c4c9c386cf190f6bcc18
khoinguyen2k <nguyeenknguyentd@gmail.com>
2022-04-07 21:11:26 +0700

    testering

--- simpleai.cpp
+++ simpleai.cpp
@@ -14,7 +14,7 @@ int readWordLen()
     cout << endl << "Enter the number characters of your secret word: ";
     cin >> wordLen;
     return wordLen;
-    
+
 }
 
 vector<string> filterWordsByLen(int wordLen, const vector<string>& vocabulary)
@@ -25,6 +25,7 @@ vector<string> filterWordsByLen(int wordLen, const vector<string>& vocabulary)
 char nextCharWhenWordIsNotInDictionary(const set<char>& selectedChars)
 {
 //Hàm trả về kí tự khi từ đoán không có trong từ điển
+   return 0;
 }
 
 map<char, int> countOccurrences(const vector<string>& candidateWords)
@@ -34,13 +35,13 @@ map<char, int> countOccurrences(const vector<string>& candidateWords)
 
 char findMostFrequentChar(const map<char, int>& occurrences, const set<char>& selectedChars)
 {
-// hàm tìm kí tự hay xuất hiện nhất  
+// hàm tìm kí tự hay xuất hiện nhất
 }
 
 /***
 Tìm kí tự xuất hiện nhiều nhất
 - Cài đặt cách 1: nếu xuất hiện nhiều lần trong 1 word thì cũng đếm nhiều lần
-- Cách 2 (ko cài ở đây): nếu xuất hiện nhiền lần trong 1 word thì chỉ đểm một lần 
+- Cách 2 (ko cài ở đây): nếu xuất hiện nhiền lần trong 1 word thì chỉ đểm một lần
 ***/
 char findBestChar(const vector<string>& candidateWords, const set<char>& selectedChars)
 {
@@ -58,7 +59,7 @@ string getWordMask(char nextChar)
 
 bool isCorrectChar(char ch, const string& mask)
 {
-// Hàm kiểm tra kí tự đã đoán có đúng hay không 
+// Hàm kiểm tra kí tự đã đoán có đúng hay không
 }
 
 bool isWholeWord(const string& mask)
@@ -66,9 +67,9 @@ bool isWholeWord(const string& mask)
 // hàm kiểm tra xem đầu vào có phải là cả từ hay không
 }
 
-bool wordConformToMask(const string& word, const string& mask, char ch) 
+bool wordConformToMask(const string& word, const string& mask, char ch)
 {
-//Hàm kiếm tra xem từ và mask có cùng dạng hay không    
+//Hàm kiếm tra xem từ và mask có cùng dạng hay không
 }
 
 vector<string> filterWordsByMask(const vector<string>& words, const string& mask, char ch)
