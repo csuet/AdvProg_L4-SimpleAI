32f831df03dc0436e444e180970013d26ad7c585
Nguyen Huu Dong <21020760@vnu.edu.vn>
2022-04-09 19:29:16 +0700

    21020760_NguyenHuuDong_commit8

--- simpleai.cpp
+++ simpleai.cpp
@@ -102,7 +102,7 @@ char findBestChar(const vector<string>& candidateWords, const set<char>& selecte
     char answer;
     //Write your code here
     map<char, int> occurrences = countOccurrences(candidateWords);
-    answer = findMostFrequentChar(selectedChars);
+    answer = findMostFrequentChar(occurrences, selectedChars);
     return answer;
 }
 
