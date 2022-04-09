adedcb37af803216f74ce4f6a4dbf6cf5fa07fcd
khoinguyen2k <nguyeenknguyentd@gmail.com>
2022-04-09 18:06:21 +0700

    try

--- simpleai.cpp
+++ simpleai.cpp
@@ -82,7 +82,7 @@ char findMostFrequentChar(const map<char, int>& occurrences, const set<char>& se
     answer =nextCharWhenWordIsNotInDictionary(selectedChars);
     int best_count = 0;
     for (auto p : occurrences)
-      //if (selectedChars.find(p.first) !=selectedChars.end() )
+      if (selectedChars.find(p.first) ==selectedChars.end() )
         if (p.second > best_count) {
             answer = p.first;
             best_count = p.second;
