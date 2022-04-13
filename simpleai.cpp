9de93d428e8cccc775db81e7a561d548d570a88a
DoDucHuy21020124 <21020124@vnu.edu.vn>
2022-04-13 10:00:47 +0700

    DoDucHuy 21020124

--- simpleai.cpp
+++ simpleai.cpp
@@ -28,6 +28,9 @@ vector<string> filterWordsByLen(int wordLen, const vector<string>& vocabulary)
 {
     vector<string> answer;
     //Write your code here
+    for ( int i = 0; i < vocabulary.size(); ++i ) {
+        if ( vocabulary[i].length() == wordLen ) answer.push_back(vocabulary[i]);
+    }
     return answer;
 }
 
