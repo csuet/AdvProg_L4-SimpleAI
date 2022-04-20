e9a310529a4fee5bb3ef83e6b70102069c095d21
Diễm Quỳnh <91684907+dquynh2811@users.noreply.github.com>
2022-04-20 22:53:18 +0700

    Update simpleai.cpp

--- simpleai.cpp
+++ simpleai.cpp
@@ -151,7 +151,7 @@ bool isWholeWord(const string& mask)
      bool answer = true;
     //Write your code here
     for(char p : mask)
-        if(p == '_')
+        if(p == '-')
         {
             answer = false;
             break ;
