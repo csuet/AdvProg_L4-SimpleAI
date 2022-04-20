00cc5ebec97dc3369e0d869171dd075171448df0
dangquangUetK66 <quangnguyenvk@gmail.com>
2022-04-20 12:09:37 +0700

    21020785 4th Fix

--- simpleai.cpp
+++ simpleai.cpp
@@ -190,8 +190,8 @@ bool wordConformToMask(const string& word, const string& mask, char ch)
     if (word.size() != mask.size()) {
         answer = false;
     } else {
-        for (int i = 0; i < mask.size(); ++ i) {
-            if (mask[i] != '-' && mask[i] != word[i]) {
+        for (int i = 0; i < word.size(); ++ i) {
+            if (word[i] != '-' && mask[i] != word[i]) {
                 answer = false;
                 break;
             }
