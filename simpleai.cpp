957499ebf07dea34a53ea3a9d128c40d1a2d8ad0
quyenhoang03 <21020787@vnu.edu.vn>
2022-04-22 17:29:46 +0700

    fix

--- simpleai.cpp
+++ simpleai.cpp
@@ -115,8 +115,7 @@ char findBestChar(const vector<string> &candidateWords, const set<char> &selecte
 {
     char answer;
     // Write your code here
-    map<char, int> occurrences = countOccurrences(candidateWords);
-    answer = findMostFrequentChar(occurrences, selectedChars);
+    answer = findMostFrequentChar(countOccurrences(candidateWords), selectedChars);
     return answer;
 }
 
