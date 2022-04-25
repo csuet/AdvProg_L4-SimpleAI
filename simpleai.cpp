8937f238f9bfd986e78c7f7b9f400081e9db1c5b
minh0010 <21020778@vnu.edu.vn>
2022-04-25 21:58:35 +0700

    update 1

--- simpleai.cpp
+++ simpleai.cpp
@@ -28,6 +28,13 @@ vector<string> filterWordsByLen(int wordLen, const vector<string>& vocabulary)
 {
     vector<string> answer;
     //Write your code here
+    for (int i = 0; i < vocabulary.size(); i++) 
+    {
+        if (vocabulary[i].size() == wordLen)
+        {
+            answer.push_back(vocabulary[i]);
+        }
+    }
     return answer;
 }
 
